2f9ca27c31ce70a2a634325bc58e1bafd96d3efc
usman ahmed <usmanahmed1100@gmail.com>
2020-12-19 17:00:57 +0500

    Insertion works but gives some errors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let creds = require("./config.js")
 app = express();
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended:false}))
 
 let db = mysql.createConnection(creds);
 
@@ -18,6 +19,24 @@ db.connect((err) => {
 app.get("/", (req, res) => {
   res.render("index.ejs")
 });
+
+app.get("/submit", (req, res) => {
+    res.render("submit.ejs")
+  });
+
+
+app.post("/submit/data/", (req, res) => {
+
+    let title = req.body.title;
+    let body_text = req.body.body_text;
+
+    let q = `INSERT INTO posts(title, body)
+             VALUES("` + title + `", "` + body_text +` ");`
+    res.render("index.ejs")
+    db.query(q);
+    res.send(title + " " + body_text)
+  });
+
 
 app.listen(8081, () => {
   console.log("Listening on localhost:8081");
