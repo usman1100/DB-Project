a68d0f2af38bd9f0b950adb33131359192979807
usman ahmed <usmanahmed1100@gmail.com>
2020-12-19 18:19:59 +0500

    Tested sessions

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 let express = require("express");
 let mysql = require("mysql");
-let creds = require("./config.js")
+let creds = require("./config.js");
+let session = require("express-session");
+
 app = express();
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({extended:false}))
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+	secret: 'pp',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: true }
+  }))
 
 let db = mysql.createConnection(creds);
 
@@ -17,46 +26,56 @@ db.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs");
 });
 
 app.get("/submit", (req, res) => {
-    res.render("submit.ejs")
-  });
-
+  res.render("submit.ejs");
+});
 
 app.post("/submit/data/", (req, res) => {
+  let title = req.body.title;
+  let body = req.body.body;
 
-    let title = req.body.title;
-    let body = req.body.body;
+  let q =
+    `INSERT INTO posts(title, body)
+             VALUES("` +
+    title +
+    `", "` +
+    body +
+    ` ");`;
+  res.render("index.ejs");
+  db.query(q);
+  res.send(title + " " + body);
+});
 
-    let q = `INSERT INTO posts(title, body)
-             VALUES("` + title + `", "` + body +` ");`
-    res.render("index.ejs")
-    db.query(q);
-    res.send(title + " " + body)
+app.get("/posts/", (req, res) => {
+  q = "SELECT * FROM posts";
+  db.query(q, [true], (error, results, fields) => {
+    if (error) {
+      res.send("error");
+    }
+
+    console.log(results);
+
+    let data = { results: results };
+
+    res.render("posts.ejs", data);
   });
+});
+
+app.get("/secret/", (req, res) => {
+
+	req.session.user = "usman"
+
+	if (!req.session.user) {
+		res.render("error.ejs");
+	}
+
+	res.render("secret.ejs", {user:req.session.user});
 
 
-  app.get("/posts/", (req, res) => {
-
-    q = "SELECT * FROM posts";
-    db.query(q, [true], (error, results, fields) => {
-
-      if(error){
-        res.send("error");
-      }
-
-      console.log(results);
-
-      let data = {"results":results};
-
-      res.render("posts.ejs", data);
-
-
-    })
-
-  })
+});
 
 app.listen(8081, () => {
   console.log("Listening on localhost:8081");
