0fd884cb9cedcc0ea4243b3c1612c61a27634d9b
usman ahmed <usmanahmed1100@gmail.com>
2020-12-20 19:17:40 +0500

    This time it actually works

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ let express = require("express");
 let mysql = require("mysql");
 let creds = require("./config.js");
 let session = require("express-session");
-let date_to_age = require("./utils")
+let date_to_age = require("./utils");
 
 let app = express();
 
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-	secret: 'pp',
-	resave: false,
-	saveUninitialized: true,
-	cookie: { secure: true }
-  }))
+app.use(
+  session({
+    secret: "pp",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
 let db = mysql.createConnection(creds);
-
 
 db.connect((err) => {
   if (err) {
@@ -27,70 +27,63 @@ db.connect((err) => {
   console.log("Connected");
 });
 
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.render("index.ejs");
-})
+});
 
 app.get("/register", (req, res) => {
-  res.render("register.ejs")
-})
+  res.render("register.ejs");
+});
 
 app.get("/error", (req, res) => {
-  res.render("error.ejs")
-})
+  res.render("error.ejs");
+});
 
 app.get("/success", (req, res) => {
-  res.render("success.ejs")
-})
-
-
-
-
+  res.render("success.ejs");
+});
 
 app.post("/register/post", (req, res) => {
-
   console.log(req.body);
   let username = req.body.user_name;
-  // db.query("SELECT user_name FROM users", [true], (errors, results, fields) => {
+  let found = false;
+  db.query("SELECT user_name FROM users", [true], (errors, results, fields) => {
+    console.log(username);
+    console.log(results);
 
-  //   console.log(username);
-  //   console.log(results);
+    let usernames = results.map((user) => user.user_name);
 
-  // })
-
-  let password = req.body.password,
-      email = req.body.email,
-      dob = req.body.dob,
-      age = 0,
-      gender = req.body.gender;
-
-  let q = `INSERT INTO users(user_name, email, pass, dob, age, gender)
-           VALUES("`
-           + username + `", "`
-           + email + `", "` 
-           + password + `", "`
-           + dob + `", "`
-           + age.toString() + `", "`
-           + gender + 
-           `");`
-  
-  db.query(q, [true], (errors, results, fields)=>{
-
-    if(errors)
-      if(errors.code == "ER_DUP_ENTRY"  )
-        res.redirect("/error")
-
-
-    else
+    if (usernames.includes(username)) {
+      res.redirect("/error");
+    } 
+    else 
     {
-      res.redirect("/success")
+      let password = req.body.password,
+        email = req.body.email,
+        dob = req.body.dob,
+        age = 0,
+        gender = req.body.gender;
+
+      let q =
+        `INSERT INTO users(user_name, email, pass, dob, age, gender)
+           VALUES("` +
+        username +
+        `", "` +
+        email +
+        `", "` +
+        password +
+        `", "` +
+        dob +
+        `", "` +
+        age.toString() +
+        `", "` +
+        gender +
+        `");`;
+
+      res.redirect("/success");
     }
-
-    res.redirect("/success")
   });
-
-})
+});
 
 // app.post("/submit/data/", (req, res) => {
 //   let title = req.body.title;
@@ -122,7 +115,6 @@ app.post("/register/post", (req, res) => {
 //     res.render("posts.ejs", data);
 //   });
 // });
-
 
 app.listen(8081, () => {
   console.log("Listening on localhost:8081");
