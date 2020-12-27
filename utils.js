0de5479896e70e047ff44ed3f2d1e4cc179b0425
usman ahmed <usmanahmed1100@gmail.com>
2020-12-27 16:27:44 +0500

    Removed useless functions

--- utils.js
+++ utils.js
@@ -32,32 +32,7 @@ const get_age = dob => {
 
 }
 
-const clean_sql = (str) => {
-    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
-        switch (char) {
-            case "\0":
-                return "\\0";
-            case "\x08":
-                return "\\b";
-            case "\x09":
-                return "\\t";
-            case "\x1a":
-                return "\\z";
-            case "\n":
-                return "\\n";
-            case "\r":
-                return "\\r";
-            case "\"":
-            case "'":
-            case "\\":
-            case "%":
-                return "\\"+char; // prepends a backslash to backslash, percent,
-                                  // and double/single quotes
-            default:
-                return char;
-        }
-    });
-}
+
 
 const separate_post_comments = (results) => {
 
@@ -80,6 +55,5 @@ module.exports = {
     get_current_date: get_current_date,
     get_formated_date: get_formated_date,
     get_age:get_age,
-    clean_sql:clean_sql,
     separate_post_comments:separate_post_comments
 };
